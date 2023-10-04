53bf354a5bc1b3d04f1eb69a924f705b1fc95a08
Hemant Karode <115488682+karodehemant9@users.noreply.github.com>
2023-10-04 10:14:35 +0530

    Add files via upload
    writing and reading messages to and from the file and showing it at the top of the form.

--- app.js
+++ app.js
@@ -1,40 +1,57 @@
 const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((request, response) => {
+  const url = request.url;
+  const method = request.method;
+  let fetchedData;
+
+  const filePath = 'message.txt';
 
 
-const server = http.createServer(function(request, response){
-    const url = request.url;
-    if(url === '/home')
-    {
-        response.setHeader('Content-Type','text/html');
-        response.write('<html>');
-        response.write('<head><title>My first page</head></title>');
-        response.write('<body><h1>Welcome home</h1></body>');
-        response.write('</html>');
+  function readFileAndSendResponse(filePath, response) {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        console.error('Error reading the file:', err);
+        response.write('<h3>Error reading text from file</h3>');
+      } 
+      else {
+        response.write('<h3>'+ data + '</h3>');
+      }
+      response.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+      response.write('</html>');
+      response.end();
+    });
+  }
+  
+
+
+  if (url === '/') {
+    response.write('<html>');
+    response.write('<head><title>Enter Message</title><head>');
+    response.write('<body>');
+    readFileAndSendResponse(filePath, response);
+  }
+
+  if (url === '/message' && method === 'POST') {
+    const body = [];
+
+    request.on('data', (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+
+    request.on('end', (err) => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFile('message.txt', message , () =>{
+        response.statusCode = 302;
+        response.setHeader('Location', '/');
         return response.end();
-    } 
-    else if(url === '/about')
-    {
-        response.setHeader('Content-Type','text/html');
-        response.write('<html>');
-        response.write('<head><title>My first page</head></title>');
-        response.write('<body><h1>Welcome to about us</h1></body>');
-        response.write('</html>');
-        return response.end();
-    } 
-    else if(url === '/node')
-    {
-        response.setHeader('Content-Type','text/html');
-        response.write('<html>');
-        response.write('<head><title>My first page</head></title>');
-        response.write('<body><h1>Welcome to my Node Js project</h1></body>');
-        response.write('</html>');
-        return response.end();
-    }       
+      });    
+    }); 
+  }
 });
 
 server.listen(3000);
-
-
-
-
 
