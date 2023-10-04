5ab740be21438ae74d30ae473b801a6b6eb89751
Hemant Karode <115488682+karodehemant9@users.noreply.github.com>
2023-10-04 08:36:25 +0530

    Add files via upload
    creating multiple routes and sending different responses

--- app.js
+++ app.js
@@ -2,10 +2,37 @@ const http = require('http');
 
 
 const server = http.createServer(function(request, response){
-    console.log('Hemant Karode');
+    const url = request.url;
+    if(url === '/home')
+    {
+        response.setHeader('Content-Type','text/html');
+        response.write('<html>');
+        response.write('<head><title>My first page</head></title>');
+        response.write('<body><h1>Welcome home</h1></body>');
+        response.write('</html>');
+        return response.end();
+    } 
+    else if(url === '/about')
+    {
+        response.setHeader('Content-Type','text/html');
+        response.write('<html>');
+        response.write('<head><title>My first page</head></title>');
+        response.write('<body><h1>Welcome to about us</h1></body>');
+        response.write('</html>');
+        return response.end();
+    } 
+    else if(url === '/node')
+    {
+        response.setHeader('Content-Type','text/html');
+        response.write('<html>');
+        response.write('<head><title>My first page</head></title>');
+        response.write('<body><h1>Welcome to my Node Js project</h1></body>');
+        response.write('</html>');
+        return response.end();
+    }       
 });
 
-server.listen(4000);
+server.listen(3000);
 
 
 
